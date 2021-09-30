36299ce0fba247e6c69795e7e34511e6daaef0d2
AouimerRamzi <AouimerRamziP@hotmail.com>
2021-09-30 22:34:43 +0200

    oui

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function complete(){
 // Get Quote From API
 async function getQuote(){
     loading();
-    const proxyUrl = 'https://git.heroku.com/agile-beyond-03486.git/'
+    const proxyUrl = 'https://agile-beyond-03486.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiUrl);
