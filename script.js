fbd4f17af6973f4655327a6c7ee123a1a7989a01
AouimerRamzi <AouimerRamziP@hotmail.com>
2021-09-30 23:11:36 +0200

    ZIDFHAZUIODHAZOD

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
@@ -19,7 +20,7 @@ function complete(){
 // Get Quote From API
 async function getQuote(){
     loading();
-    const proxyUrl = 'https://git.heroku.com/agile-beyond-03486.git/'
+    const proxyUrl = 'https://frozen-sea-89761.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiUrl);
