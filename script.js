6209ff41216b49696bc2d8b39744d4e2e689d210
AouimerRamzi <AouimerRamziP@hotmail.com>
2021-09-30 23:18:12 +0200

    ZIAAIODFHAZEIODFAZHFIOAZBHFIOAZEB

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function complete(){
 // Get Quote From API
 async function getQuote(){
     loading();
-    const proxyUrl = 'https://frozen-sea-89761.herokuapp.com/'
+    const proxyUrl = 'https://agile-beyond-03486.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiUrl);
