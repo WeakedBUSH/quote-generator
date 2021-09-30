904fb2ceef98805dd14ba9db1a56569fc1064dc1
AouimerRamzi <AouimerRamziP@hotmail.com>
2021-09-30 23:02:18 +0200

    oui2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
 
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
