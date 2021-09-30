cdfc7bb1da2a035233bc0ebb04bb4bd0252bfe24
AouimerRamzi <AouimerRamziP@hotmail.com>
2021-09-30 23:54:43 +0200

    hmmm depression

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function complete(){
 // Get Quote From API
 async function getQuote(){
     loading();
-    const proxyUrl = 'https://fast-refuge-67377.herokuapp.com/'
+    const proxyUrl = 'https://morning-basin-99072.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiUrl);
